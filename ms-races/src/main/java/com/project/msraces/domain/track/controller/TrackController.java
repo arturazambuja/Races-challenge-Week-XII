2e3646d9e723a4b3912e2aaa90a14554aefb80e3
Artur Azambuja <arturantoniolli@gmail.com>
2023-09-11 23:52:36 -0300

    feat: created Get all, Get by id and Post methods from tracks on ms-races

--- ms-races/src/main/java/com/project/msraces/domain/track/controller/TrackController.java
+++ ms-races/src/main/java/com/project/msraces/domain/track/controller/TrackController.java
@@ -1,9 +1,32 @@
 package com.project.msraces.domain.track.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.msraces.domain.track.dtos.TrackRequestDTO;
+import com.project.msraces.domain.track.dtos.TrackResponseDTO;
+import com.project.msraces.domain.track.exceptions.TrackNotFoundException;
+import com.project.msraces.domain.track.service.TrackService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tracks")
 public class TrackController {
+
+    private final TrackService trackService;
+
+    public TrackController(TrackService trackService) {
+        this.trackService = trackService;
+    }
+    @GetMapping
+    public List<TrackResponseDTO> getAllTracks() {
+        return trackService.getAllTracks();
+    }
+    @GetMapping("/{idTrack}")
+    public TrackResponseDTO getTrackById(@PathVariable Long idTrack) throws TrackNotFoundException {
+        return trackService.getTrackById(idTrack);
+    }
+    @PostMapping
+    public TrackResponseDTO createTrack(@RequestBody TrackRequestDTO requestDTO) {
+        return trackService.createTrack(requestDTO);
+    }
 }
--- ms-races/src/main/java/com/project/msraces/domain/track/service/TrackService.java
+++ ms-races/src/main/java/com/project/msraces/domain/track/service/TrackService.java
@@ -1,7 +1,48 @@
 package com.project.msraces.domain.track.service;
 
+import com.project.msraces.domain.track.dtos.TrackRequestDTO;
+import com.project.msraces.domain.track.dtos.TrackResponseDTO;
+import com.project.msraces.domain.track.exceptions.TrackNotFoundException;
+import com.project.msraces.domain.track.model.Track;
+import com.project.msraces.domain.track.repository.TrackRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TrackService {
+
+    private final ModelMapper modelMapper;
+    private final TrackRepository trackRepository;
+    public TrackService(ModelMapper modelMapper, TrackRepository trackRepository) {
+        this.modelMapper = modelMapper;
+        this.trackRepository = trackRepository;
+    }
+    public TrackResponseDTO convertTrackToResponseDTO(Track track) {
+        return modelMapper.map(track, TrackResponseDTO.class);
+    }
+    public TrackResponseDTO createTrack(TrackRequestDTO requestDTO) {
+
+        Track track = new Track();
+        track.setName(requestDTO.getName());
+        track.setCountry(requestDTO.getCountry());
+        track.setDate(requestDTO.getDate());
+
+        Track savedTrack = trackRepository.save(track);
+
+        return convertTrackToResponseDTO(savedTrack);
+    }
+    public List<TrackResponseDTO> getAllTracks() {
+        List<Track> tracks = trackRepository.findAll();
+        return tracks.stream()
+                .map(this::convertTrackToResponseDTO)
+                .collect(Collectors.toList());
+    }
+    public TrackResponseDTO getTrackById(Long idTrack) throws TrackNotFoundException {
+        Track track = trackRepository.findById(idTrack)
+                .orElseThrow(() -> new TrackNotFoundException("Track not found with this id"));
+        return convertTrackToResponseDTO(track);
+    }
 }
